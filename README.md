4b94726e4a504ef59d7c0004cab616af29f6730f
Marko Varghese <marko.varghese@gmail.com>
2020-11-24 00:47:34 -0600

    Updated to create schema if exists, or write to the latest schema or write to a specific schema version

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ## Run locally
 
-> please note that this repo was crearted to share an issue I'm facing. Hence the following instructions are to reproduce the issue. Until the issue ios resolved, you won't see data being moved
-
 - Stand up a dockerised kafka cluster by running 
 ```shell script
 CONFLUENT_VERSION="5.5.2" docker-compose -f docker_kafka_server/docker-compose.yml up -d --build
@@ -30,58 +28,13 @@ docker run -v $(pwd):/core -w /core -it --rm --network docker_kafka_server_defau
 
 ### Ideal result
 
-The spark application will create a topic `test123`, register the schema of a Spark dataframe with two numeric fields, and then write the records of the dataframe to the topic `test123`. You should be able to see the topic `test123` on the Topics screen of Confluent Control Center at http://localhost:9021/ ; and you should be ab le browse through the messages written into the topic. 
+The spark application will create a topic `test123`, register the schema of a Spark dataframe with two numeric fields, and then write the records of the dataframe to the topic `test123`. You should be able to see the topic `test123` on the Topics screen of Confluent Control Center at http://localhost:9021/ ; and you should be able browse through the messages written into the topic. 
 
 ### What's stopping us?
 
 Nothing; it all works!
 
-There are some minor issues. 
-
-#### Minor Issue 1
-
-If you use 
-```scala
-  val expression = allColumns.expr
-  toAvroType(expression.dataType, expression.nullable)
-```
-to generate the avro schema for the struct of all columns in the dataframe as suggested at https://github.com/AbsaOSS/ABRiS/blob/master/documentation/confluent-avro-documentation.md#generate-schema-from-data-and-register, then you will get the following runtime error
-```text
-Exception in thread "main" java.lang.UnsupportedOperationException: Cannot evaluate expression: NamePlaceholder
-        at org.apache.spark.sql.catalyst.expressions.Unevaluable.eval(Expression.scala:301)
-        at org.apache.spark.sql.catalyst.expressions.Unevaluable.eval$(Expression.scala:300)
-        at org.apache.spark.sql.catalyst.expressions.NamePlaceholder$.eval(complexTypeCreator.scala:302)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.$anonfun$names$1(complexTypeCreator.scala:377)
-        at scala.collection.immutable.List.map(List.scala:286)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.names$lzycompute(complexTypeCreator.scala:377)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.names(complexTypeCreator.scala:377)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.dataType$lzycompute(complexTypeCreator.scala:384)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.dataType(complexTypeCreator.scala:383)
-        at org.apache.spark.sql.catalyst.expressions.CreateNamedStruct.dataType(complexTypeCreator.scala:372)
-        at org.example.App$.main(App.scala:34)
-        at org.example.App.main(App.scala)
-        at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-        at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
-        at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-        at java.lang.reflect.Method.invoke(Method.java:498)
-        at org.apache.spark.deploy.JavaMainApplication.start(SparkApplication.scala:52)
-        at org.apache.spark.deploy.SparkSubmit.org$apache$spark$deploy$SparkSubmit$$runMain(SparkSubmit.scala:928)
-        at org.apache.spark.deploy.SparkSubmit.doRunMain$1(SparkSubmit.scala:180)
-        at org.apache.spark.deploy.SparkSubmit.submit(SparkSubmit.scala:203)
-        at org.apache.spark.deploy.SparkSubmit.doSubmit(SparkSubmit.scala:90)
-        at org.apache.spark.deploy.SparkSubmit$$anon$2.doSubmit(SparkSubmit.scala:1007)
-        at org.apache.spark.deploy.SparkSubmit$.main(SparkSubmit.scala:1016)
-        at org.apache.spark.deploy.SparkSubmit.main(SparkSubmit.scala)
-```
-
-##### Workaround
-Use the following script instead, to generate the schema (ref [src/main/scala/org/example/App.scala#L35-L36](blob/spark3.0.1-scala2.12-hadoop2.10.0-abris4.0.1/src/main/scala/org/example/App.scala#L35-L36) ).
-```scala
-  val tempdf = df.withColumn("allcolumns", allColumns)
-  val schema = toAvroSchema(tempdf, "allcolumns")
-```
-
-#### Minor Issue 2
+#### Minor Issue
 If you run the Spark application on a spark cluster with Hadoop 3.0.0 to 3.2.1, you will get the following runtime error
 ```text
 2020-11-14 23:32:33,626 ERROR executor.Executor: Exception in task 6.0 in stage 3.0 (TID 22)
--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,11 +1,9 @@
 package org.example
 
 import org.apache.spark.sql.avro.SchemaConverters.toAvroType
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions._
-import za.co.absa.abris.config.{AbrisConfig, ToAvroConfig}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import za.co.absa.abris.config.{AbrisConfig, ToAvroConfig, ToStrategyConfigFragment}
 import za.co.absa.abris.avro.functions.to_avro
-import za.co.absa.abris.avro.parsing.utils.AvroSchemaUtils.toAvroSchema
 import za.co.absa.abris.avro.read.confluent.SchemaManagerFactory
 import za.co.absa.abris.avro.registry.SchemaSubject
 
@@ -25,30 +23,74 @@ object App {
     val pvalues: Array[Double] = Array(0.6, 0.4, 8.9, 7.4, 2.8, 0.0)
     val rdd = spark.sparkContext.parallelize(tvalues zip pvalues)
     import spark.implicits._
-    val df = rdd.toDF("Tvalues","Pvalues")
+    var df = rdd.toDF("Tvalues", "Pvalues")
+    df.createOrReplaceTempView("df")
+    df = spark.sql("SELECT named_struct('Tvalues', Tvalues, 'Pvalues', Pvalues) as value, uuid() as key, (SELECT COLLECT_SET(named_struct('hello', 'world'))) as headers FROM df")
     df.show
     val topic: String = "test123"
     val schemaRegistry: String = "http://schema-registry:8081"
-    var allColumns = struct(df.columns.head, df.columns.tail: _*)
-    //val expression = allColumns.expr
-    //val schema = toAvroType(expression.dataType, expression.nullable)
-    val tempdf = df.withColumn("allcolumns", allColumns)
-    val schema = toAvroSchema(tempdf, "allcolumns")
-    println("avro schema = " + schema.toString())
+    val broker = "broker:29092"
+    //create the schema
+    dataFrameToKafka(df = df, spark = spark, valueField = "value", topic = topic, kafkaBroker = broker, schemaRegistryUrl = schemaRegistry, keyField = Some("key"), headerField = Some("headers"))
+    //write to the latest schema
+    dataFrameToKafka(df = df, spark = spark, valueField = "value", topic = topic, kafkaBroker = broker, schemaRegistryUrl = schemaRegistry, keyField = Some("key"), headerField = Some("headers"))
+    //write to a specifci schema
+    dataFrameToKafka(df = df, spark = spark, valueField = "value", topic = topic, kafkaBroker = broker, schemaRegistryUrl = schemaRegistry, keyField = Some("key"), headerField = Some("headers"), valueSchemaVersion = Some(1), keySchemaVersion = Some(1))
+  }
 
-    val subject = SchemaSubject.usingTopicNameStrategy(topic, isKey=false) // Use isKey=true for the key schema and isKey=false for the value schema
-    val schemaRegistryClientConfig = Map(AbrisConfig.SCHEMA_REGISTRY_URL -> schemaRegistry)
-    val schemaManager = SchemaManagerFactory.create(schemaRegistryClientConfig)
-    val schemaId = schemaManager.register(subject, schema)
-    val toAvroConfig: ToAvroConfig = AbrisConfig
-      .toConfluentAvro
-      .downloadSchemaById(schemaId)
-      .usingSchemaRegistry(schemaRegistry)
-    df.select(to_avro(allColumns, toAvroConfig) as 'value)
-      .write
-      .option("kafka.bootstrap.servers", "broker:29092")
+  def dataFrameToKafka(spark: SparkSession, df: DataFrame, valueField: String, topic: String, kafkaBroker: String, schemaRegistryUrl: String, valueSchemaVersion: Option[Int] = None, keyField: Option[String] = None, keySchemaVersion: Option[Int] = None, headerField: Option[String] = None): Unit = {
+    var dfavro = spark.emptyDataFrame
+    var columnsToSelect = Seq(to_avro(df.col(valueField), GetToAvroConfig(topic = topic, schemaRegistryUrl = schemaRegistryUrl, dfColumn = df.col(valueField), schemaVersion = valueSchemaVersion, isKey = false)) as 'value)
+    if (!keyField.isEmpty) {
+      val keyFieldCol = df.col(keyField.get)
+      columnsToSelect = columnsToSelect ++ Seq(to_avro(keyFieldCol, GetToAvroConfig(topic = topic, schemaRegistryUrl = schemaRegistryUrl, dfColumn = keyFieldCol, schemaVersion = keySchemaVersion, isKey = true)) as 'key)
+    }
+    if (!headerField.isEmpty) {
+      columnsToSelect = columnsToSelect ++ Seq(df.col(headerField.get) as 'header)
+    }
+    dfavro = df.select(columnsToSelect: _*)
+    dfavro.printSchema()
+    dfavro.write
+      .option("kafka.bootstrap.servers", kafkaBroker)
       .option("topic", topic)
+      .option("includeHeaders", (!headerField.isEmpty).toString)
       .format("kafka")
       .save()
+  }
+
+  def GetToAvroConfig(topic: String, schemaRegistryUrl: String, dfColumn: Column, schemaVersion: Option[Int] = None, isKey: Boolean = false): ToAvroConfig = {
+    //get the specified schema version
+    //if not specified, then get the latest schema from Schema Registry
+    //if the topic does not have a schema then create and register the schema
+    //applies to both key and value
+    val subject = SchemaSubject.usingTopicNameStrategy(topic, isKey = isKey) // Use isKey=true for the key schema and isKey=false for the value schema
+    val schemaRegistryClientConfig = Map(AbrisConfig.SCHEMA_REGISTRY_URL -> schemaRegistryUrl)
+    val schemaManager = SchemaManagerFactory.create(schemaRegistryClientConfig)
+    var toAvroConfig: ToAvroConfig = null
+    if (schemaManager.exists(subject)) {
+      val avroConfigFragment = AbrisConfig
+        .toConfluentAvro
+      var toStrategyConfigFragment: ToStrategyConfigFragment = null
+      if (schemaVersion.isEmpty) {
+        toStrategyConfigFragment = avroConfigFragment.downloadSchemaByLatestVersion
+      }
+      else {
+        toStrategyConfigFragment = avroConfigFragment.downloadSchemaByVersion(schemaVersion.get)
+      }
+      toAvroConfig = toStrategyConfigFragment
+        .andTopicNameStrategy(topic, isKey = isKey)
+        .usingSchemaRegistry(schemaRegistryUrl)
+    }
+    else {
+      val expression = dfColumn.expr
+      val schema = toAvroType(expression.dataType, expression.nullable)
+      println("avro schema = " + schema.toString())
+      val schemaId = schemaManager.register(subject, schema)
+      toAvroConfig = AbrisConfig
+        .toConfluentAvro
+        .downloadSchemaById(schemaId)
+        .usingSchemaRegistry(schemaRegistryUrl)
+    }
+    toAvroConfig
   }
 }
